4bb3b7c4558147d041c4620edefefd073c6a3278
Bukunmi1234 <oluwabukunmiodewumi@gmail.com>
2022-10-14 05:31:09 -0700

    nice

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
+
+
 /**
  *
  *  * main - Entry point
- *  * programming description
- *  */
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-int main() 
+int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
