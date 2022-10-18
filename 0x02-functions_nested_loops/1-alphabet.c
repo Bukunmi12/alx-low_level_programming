eec3039a38a7a34a496dd8d5e88ad487e822439d
Bukunmi1234 <oluwabukunmiodewumi@gmail.com>
2022-10-18 11:08:40 -0700

    good day

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "0. _putchar"
+
+
+
+void print_alphabet_x10(void);
+{
+ *
+ *  * main - Entry point
+ *
+ *   * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ *    *
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      * Return: Always 0 (Succes)
+ *
+ *       */
+
+void print_alphabet(void)
+
+{
