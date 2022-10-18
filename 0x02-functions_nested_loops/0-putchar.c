739707b977b5b0fe511d61c821d60884b899ba83
Bukunmi1234 <oluwabukunmiodewumi@gmail.com>
2022-10-18 16:13:17 -0700

    good day

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
-#include "main.h";
+#include "main.h"
+
+
 
 /**
  *
@@ -9,17 +11,30 @@
  *    */
 
 int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
 
-	return (0);
+{
+
+		_putchar('_');
+
+			_putchar('p');
+
+				_putchar('u');
+
+					_putchar('t');
+
+						_putchar('c');
+
+							_putchar('h');
+
+								_putchar('a');
+
+									_putchar('r');
+
+										_putchar('\n');
+
+
+
+											return (0);
+
 }
 
