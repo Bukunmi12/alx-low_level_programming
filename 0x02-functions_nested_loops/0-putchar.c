04fae8477f220fbfe96a75503b6f1aced799bdde
Bukunmi1234 <oluwabukunmiodewumi@gmail.com>
2022-10-19 17:34:20 -0700

    hard

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,36 +1,39 @@
 #include "main.h"
+
+
+
 /**
  *
- * * main - entry point
+ *  * main -  prints _putchar, followed by a new line
  *
- * *
- * *
- * * Return: 0
- *
+ *   * Return: Always 0 (Success)
  *
  */
+
 int main(void)
 
 {
 
-                _putchar('_');
+	_putchar('_');
 
-		_putchar('p');
+	_putchar('p');
 
-		_putchar('u');
+	_putchar('u');
 
-		_putchar('t');
+	_putchar('t');
 
-                _putchar('c');
+	_putchar('c');
 
-		_putchar('h');
+	_putchar('h');
 
-		_putchar('a');
+	_putchar('a');
 
-		_putchar('r');
+	_putchar('r');
 
-		_putchar('\n');
+	_putchar('\n');
 
-		return (0);
+
+
+	return (0);
 
 }
