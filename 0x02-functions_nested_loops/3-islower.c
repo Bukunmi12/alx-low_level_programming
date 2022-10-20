75bf2d5e4f99eb802b991cb3df0a1667bb0e5d27
Bukunmi1234 <oluwabukunmiodewumi@gmail.com>
2022-10-19 21:36:23 -0700

    jojo

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,7 @@
 
 /**
  *  * _islower - checks for lowercase character
- *  Description: 'islower'
- *
+ *    Description: 'parameter or member c'
  *    * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
