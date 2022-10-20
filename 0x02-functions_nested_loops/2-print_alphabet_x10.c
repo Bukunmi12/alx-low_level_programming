fa1100e9b2685619bc99ce161429c890a34cf341
Bukunmi1234 <oluwabukunmiodewumi@gmail.com>
2022-10-19 19:01:19 -0700

    gooooood

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,135 @@
+#include "main.h"
+
+
+
+iilphabet_x10 - prints 10 times the alphabet, in lowercase,
+	      
+iilphabet_x10 - prints 10 times the alphabet, in lowercase,
+	      
+	       * followed by a new line
+	       *
+	       *  */
+	       
+	       void print_alphabet_x10(void)
+	
+{
+	
+		char ch;
+		
+			int i;
+			
+			
+			
+				i = 0;
+				
+				
+				
+					while (i < 10)
+						
+							{
+								
+										ch = 'a';
+										
+												while (ch <= 'z')
+													
+															{
+																
+																			_putchar(ch);
+																			
+																						ch++;
+																						
+																								}
+												
+														_putchar('\n');
+														
+																i++;
+																
+																	}
+					
+}nclude <stdio.h>
+	       * followed by a new line
+	       *
+	       *  */
+	       
+	       void print_alphabet_x10(void)
+	
+{
+	
+		char ch;
+		
+			int i;
+			
+			
+			
+				i = 0;
+				
+				
+				
+					while (i < 10)
+						
+							{
+								
+										ch = 'a';
+										
+												while (ch <= 'z')
+													
+															{
+																
+																			_putchar(ch);
+																			
+																						ch++;
+																						
+																								}
+												
+														_putchar('\n');
+														
+																i++;
+																
+																	}
+					
+}nclude <stdio.h>
+/**
+ *
+ *  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ *
+ *   * followed by a new line
+ *
+ *    */
+
+void print_alphabet_x10(void)
+
+{
+
+		char ch;
+
+			int i;
+
+
+
+				i = 0;
+
+
+
+					while (i < 10)
+
+							{
+
+										ch = 'a';
+
+												while (ch <= 'z')
+
+															{
+
+																			_putchar(ch);
+
+																						ch++;
+
+																								}
+
+														_putchar('\n');
+
+																i++;
+
+																	}
 
 }
