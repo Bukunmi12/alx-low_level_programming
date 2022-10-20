376c04eeebb94fe6d644ca81a813f3be7a068e68
Bukunmi1234 <oluwabukunmiodewumi@gmail.com>
2022-10-19 21:28:25 -0700

    goodboy

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- *
  *  * _islower - checks for lowercase character
  *
- *   * @c: the character to check
+ *  Description: 'islower'
  *
  *    * Return: 1 if c is lowercase, 0 otherwise
- *
- *     */
+ */
 int _islower(int c)
 
 {
