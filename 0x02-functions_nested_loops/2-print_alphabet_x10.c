71633d6813a9b01c3174db3ba589f919ee73c8d7
Bukunmi1234 <oluwabukunmiodewumi@gmail.com>
2022-10-19 21:17:30 -0700

    sing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,135 +1,30 @@
 #include "main.h"
 
-
-
-iilphabet_x10 - prints 10 times the alphabet, in lowercase,
-	      
-iilphabet_x10 - prints 10 times the alphabet, in lowercase,
-	      
-	       * followed by a new line
-	       *
-	       *  */
-	       
-	       void print_alphabet_x10(void)
-	
-{
-	
-		char ch;
-		
-			int i;
-			
-			
-			
-				i = 0;
-				
-				
-				
-					while (i < 10)
-						
-							{
-								
-										ch = 'a';
-										
-												while (ch <= 'z')
-													
-															{
-																
-																			_putchar(ch);
-																			
-																						ch++;
-																						
-																								}
-												
-														_putchar('\n');
-														
-																i++;
-																
-																	}
-					
-}nclude <stdio.h>
-	       * followed by a new line
-	       *
-	       *  */
-	       
-	       void print_alphabet_x10(void)
-	
-{
-	
-		char ch;
-		
-			int i;
-			
-			
-			
-				i = 0;
-				
-				
-				
-					while (i < 10)
-						
-							{
-								
-										ch = 'a';
-										
-												while (ch <= 'z')
-													
-															{
-																
-																			_putchar(ch);
-																			
-																						ch++;
-																						
-																								}
-												
-														_putchar('\n');
-														
-																i++;
-																
-																	}
-					
-}nclude <stdio.h>
 /**
- *
  *  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  *
- *   * followed by a new line
+ *  Description: 'print_alphabet_x10 '
  *
- *    */
-
+ *   * followed by a new line
+ */
 void print_alphabet_x10(void)
 
 {
 
 		char ch;
+		int i;
 
-			int i;
+		i = 0;
 
-
-
-				i = 0;
-
-
-
-					while (i < 10)
-
-							{
-
-										ch = 'a';
-
-												while (ch <= 'z')
-
-															{
-
-																			_putchar(ch);
-
-																						ch++;
-
-																								}
-
-														_putchar('\n');
-
-																i++;
-
-																	}
-
+		while (i < 10)
+		{
+			ch = 'a';
+			while (ch <= 'z')
+			{
+				_putchar(ch);
+				ch++;
+			}
+			_putchar('\n');
+			i++;
+		}
 }
